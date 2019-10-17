cd30bcfa0329dfe8af47190e3ef476ebb5dda555
VenomRishi <mhatrerishikesh@gmail.com>
2019-10-17 11:23:10 +0530

    course-api-data project having the code for jpa at start we have to add only jpa and apache derby in pom xml but in this it uses apache database to store values

--- course-api-data/src/main/java/com/bridgelabz/courseapidata/Topic.java
+++ course-api-data/src/main/java/com/bridgelabz/courseapidata/Topic.java
@@ -1,8 +1,11 @@
-package com.bridgelabz.springbootstarter.topic;
+package com.bridgelabz.courseapidata;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Topic {
-	
+	@Id
 	private String id;
 	private String name;
 	private String description;
---
+++ course-api-data/src/main/java/com/bridgelabz/courseapidata/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.courseapidata;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+}

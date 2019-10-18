1c7cc6c5dd235a3bedb93c295cfc84eb11fcd1cc
VenomRishi <mhatrerishikesh@gmail.com>
2019-10-18 10:40:07 +0530

    spring mvc pattern is used and crud operation is implemented in project

--- spring-mvc-crud/src/main/java/com/bridgelabz/crud/AppController.java
+++ spring-mvc-crud/src/main/java/com/bridgelabz/crud/AppController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -24,5 +28,25 @@ public class AppController {
 		Product product = new Product();
 		model.addAttribute("product", product);
 		return "new_product";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		productService.save(product);
+		return "redirect:/";
+	}
+
+	@RequestMapping("/edit/{id}")
+	public ModelAndView EditProductFrom(@PathVariable(name = "id") Long id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+		Product product = productService.get(id);
+		mav.addObject("product", product);
+		return mav;
+	}
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name="id")Long id) {
+		productService.delete(id);
+		return "redirect:/";
 	}
 }
